268309e7e11f2684d262bedfc500c0adb797e759
Dabizu <dabizu100@gmail.com>
2021-08-16 16:13:31 -0500

    actualizacion

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app=express();
+const port = process.env.PORT || 3000;
 
 app.get("/",(req,res)=>{
     res.send("hola");
 });
 
-app.listen(3000,()=>{console.log("se inicio el servidor");});
+app.listen(port,()=>{console.log("se inicio el servidor");});
