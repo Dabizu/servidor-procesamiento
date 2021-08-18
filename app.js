13cb85c43706b90afab33f2fb095540fc5261802
Dabizu <dabizu100@gmail.com>
2021-08-18 17:58:31 -0500

    cors

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app=express();
 const multer =require('multer');
+const cors=require("cors");
 const port = process.env.PORT || 3000;
 const fs=require('fs')
+app.use(cors());
 
 app.get("/",(req,res)=>{
     res.send("hola");
