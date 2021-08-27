fbd3dfd1c7de6d4b2ba7b1d0af31f6421bb93cb1
Dabizu <dabizu100@gmail.com>
2021-08-27 03:40:29 -0500

    actualizacion

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
+/*
+const express =require('express');
+const app=express();
+const cors=require("cors");
+const fileUpload = require('express-fileupload');
+const port = process.env.PORT || 3000;
+app.use(cors());
+app.use(fileUpload());
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname+"/pagina.html");
+})
+app.post("/imagen",(req,res)=>{
+    let archivo=req.files.archivo
+    archivo.mv('./documentos/'+archivo.name,err => {
+        if(err)return res.status(500).send({message : err})
+
+        return res.status(200).send({message : "File Upload"})
+    })
+    //res.send("se paso el archivo");
+});
+
+app.listen(port,()=>{console.log("se inicio servidor")});
+*/
+
+
+
+
 const express = require('express');
 const app=express();
 const multer =require('multer');
@@ -7,7 +34,7 @@ const fs=require('fs')
 app.use(cors());
 
 app.get("/",(req,res)=>{
-    res.send("hola");
+    res.sendFile(__dirname+"juego.html");
 });
 
 app.locals.nombreArchivo='unnamed.jpg';
@@ -20,8 +47,7 @@ const storageArchivo = multer.diskStorage({
     }
 });
 const uploadArchivo = multer({
-    storage: storageArchivo
-    /*,
+    storage: storageArchivo,
     fileFilter: function(req, file, cb) {
         if (!file.originalname.match(/\.(pdf)$/)) {
             app.locals.banderaTipoArchivo = "false";
@@ -29,7 +55,7 @@ const uploadArchivo = multer({
         }
         app.locals.banderaTipoArchivo = "true";
         cb(null, true);
-    }*/
+    }
 });
 app.get("/imagen",uploadArchivo.single('archivo'),(req,res)=>{
     res.send("se paso el archivo");
@@ -51,3 +77,4 @@ app.get("/eliminar",(req,res)=>{
     }
 })
 app.listen(port,()=>{console.log("se inicio el servidor");});
+
